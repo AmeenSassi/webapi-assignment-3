272e1426c415f24fbfd1a23144f3cc5ea1cb61f2
Ameen Sassi <ameensassi@gmail.com>
2018-03-14 21:37:40 -0600

    Removed Movie schema from User.js

--- Users.js
+++ Users.js
@@ -33,25 +33,5 @@ UserSchema.methods.comparePassword = function(password, callback) {
        callback(isMatch) ;
     });
 };
-
-var ActorSchema = new Schema({
-    actorname: {type: String, required: true, index: {unique: false}},
-    charactername: {type: String, required: true, index: {unique: false}}
-});
-
-var MovieSchema = new Schema({
-    title: { type: String, required: true, index: { unique: false }},
-    year: {type: Number, required: true, index: {unique: true}},
-    genre: {type: String, enum: ["Action", "Adventure", "Comedy", "Drama", "Fantasy", "Horror", "Mystery", "Thriller", "Western"]},
-    actors: {type: [ActorSchema], min: 3, max: 3}
-});
-
-MovieSchema.pre('save',function(next){
-    if(this.actors.length < 3){
-        return next(new Error('Fewer than 3 actors'))
-    }
-    next()
-})
-
 // return the model
 module.exports = mongoose.model('User', UserSchema);
